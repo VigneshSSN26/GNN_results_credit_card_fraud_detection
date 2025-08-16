024f25881a8576b4e37f5e13e0f70b0231970cef
Vignesh <157633158+VigneshSSN26@users.noreply.github.com>
2025-08-17 01:15:57 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
-// frontend/tailwind.config.js
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // This line scans all your React components
   ],
   theme: {
     extend: {
       colors: {
-      
+        'brand-dark': '#0D1117',
+        'brand-light-dark': '#161B22',
+        'brand-blue': '#2F81F7',
+        'brand-cyan': '#39D3F7',
+        'brand-border': '#30363D',
       }
     },
   },
